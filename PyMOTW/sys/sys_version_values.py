a803fca8c3fadc7d87ed935ac6bc973fb9478a36
Mariano Reingart <reingart@gmail.com>
2015-10-02 03:34:55 -0300

    Upgrade python2/python3 sys interpreter example (was 2.6/2.7)

--- PyMOTW/sys/sys_version_values.py
+++ PyMOTW/sys/sys_version_values.py
@@ -9,10 +9,11 @@
 
 import sys
 
-print 'Version info:'
-print
-print 'sys.version      =', repr(sys.version)
-print 'sys.version_info =', sys.version_info
-print 'sys.hexversion   =', hex(sys.hexversion)
-print 'sys.subversion   =', sys.subversion
-print 'sys.api_version  =', sys.api_version
+print('Version info:')
+print()
+print('sys.version      =', repr(sys.version))
+print('sys.version_info =', sys.version_info)
+print('sys.hexversion   =', hex(sys.hexversion))
+if sys.version_info[0] < 3:
+    print('sys.subversion   =', sys.subversion)
+print('sys.api_version  =', sys.api_version)

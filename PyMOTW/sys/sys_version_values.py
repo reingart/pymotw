7ec3e41d41ad2c9f46dcf6175446d1afa35b72f6
Mariano Reingart <reingart@gmail.com>
2015-10-03 00:19:32 -0300

    Fix sys version due py3 and last merge

--- PyMOTW/sys/sys_version_values.py
+++ PyMOTW/sys/sys_version_values.py
@@ -12,8 +12,8 @@ import sys
 print('Version info:')
 print('')
 print('sys.version      = %s' % repr(sys.version))
-print('sys.version_info = %s' % sys.version_info)
+print('sys.version_info = %s' % repr(sys.version_info))
 print('sys.hexversion   = %s' % hex(sys.hexversion))
 if sys.version_info[0] < 3:
-    print('sys.subversion   = %s' % sys.subversion)
+    print('sys.subversion   = %s' % repr(sys.subversion))
 print('sys.api_version  = %s' % sys.api_version)
